f5e7c200a6bda01d43b1e07c300a05cab4e2dbbf
TerrenceMM2 <terrencemm2@gmail.com>
2019-09-15 21:38:51 -0500

    react build

---
+++ client/build/precache-manifest.40b13f611a67e07ebe3474bbf1ef7e49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8783c47d9cba4e13c029",
+    "url": "/static/js/main.f5ce3715.chunk.js"
+  },
+  {
+    "revision": "08dd1411fdd88a7f0742",
+    "url": "/static/js/2.697d8a58.chunk.js"
+  },
+  {
+    "revision": "8783c47d9cba4e13c029",
+    "url": "/static/css/main.f9f9a2a3.chunk.css"
+  },
+  {
+    "revision": "dc15ad3355c0b0ef890c6e2ada73012e",
+    "url": "/index.html"
+  }
+];
