b8127fd2a4712f930080fdccbadc38037f28cb59
TerrenceMM2 <terrencemm2@gmail.com>
2019-09-16 17:23:28 -0500

    reverts routes for heroku deployment

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@ var router = express.Router();
 
 // URLs that come in from the the room path ("/") will be routed to the appropriate controller based on the path the follows and the method used.
 router.use('/', require('./app'));
-router.use('/search', require('./api/books'));
-router.use('/saved', require('./api/books'));
 router.use('/api/books', require('./api/books'));
 // If any other URL path is entered, the browser will display a 404 page.
 router.use('*', function(req, res){
