8d8ee07633777f6c55f2e32023de8c13dbdd00fc
TerrenceMM2 <terrencemm2@gmail.com>
2019-09-16 10:58:48 -0500

    addresses heroku deployment page navigation

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,14 @@ var router = express.Router();
 
 // URLs that come in from the the room path ("/") will be routed to the appropriate controller based on the path the follows and the method used.
 router.use('/', require('./app'));
+router.use('/search', require('./api/books'));
+router.use('/saved', require('./api/books'));
 router.use('/api/books', require('./api/books'));
 // If any other URL path is entered, the browser will display a 404 page.
-// router.use('*', function(req, res){
-//     res.json({
-//         message : "Whoops ... 404."
-//     });
-// });
+router.use('*', function(req, res){
+    res.json({
+        message : "Whoops ... 404."
+    });
+});
 
 module.exports = router;
